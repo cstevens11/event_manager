833ffe4fcc93497f1fa1dfebbcb6df9db36133ff
cstevens <colestevens24@gmail.com>
2023-05-16 16:25:16 -0700

    safety save

---
+++ lib/event_manager.rb
@@ -0,0 +1,25 @@
+require 'csv'
+puts 'EventManager initialized.'
+
+# contents = CSV.open('event_attendees.csv', headers: true)
+# contents.each do |row|
+#   name = row[2]
+#   puts name
+# end
+
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+  puts "#{name} #{zipcode}"
+end
